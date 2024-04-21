b1eb39c4226046e5465d3d3db98035c5ca953ab5
mvs <mityaginvs@ya.ru>
2024-04-21 19:34:30 +0300

    собраны пути и сверстана шапка

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Main from '../Main';
+
+function App() {
+
+  return (
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Main/Header';
+
+function Main() {
+    return (
+      <main className="page">
+        <Header />
+      </main>
+    );
+  }
+
+  export default Main;
